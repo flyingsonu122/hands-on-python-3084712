58da72e79a7f50cfc98977b425c55446b81a8875
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2023-01-08 12:31:07 +0000

    new

--- Ex_Files/04_01_begin/main.py
+++ Ex_Files/04_01_begin/main.py
@@ -4,3 +4,9 @@ response = requests.get(
     "http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json")
 
 last_twenty_years = response.json()[1][:20]
+
+print(last_twenty_years)
+
+for year in last_twenty_years:
+    display_width = year["value"] // 10_00_0000
+    print(year["date"], "=" * display_width)
